c5aaff4782fd548bb45d09cb35c0ee9514b88862
JurassicSPb[credential] <Jurassic5@yandex.ru>
2017-07-26 09:48:19 +0300

    task1 - test added

--- src/test/java/com/epam/hackerrank/data_structures/task1/ArraysLeftRotationTest.java
+++ src/test/java/com/epam/hackerrank/data_structures/task1/ArraysLeftRotationTest.java
@@ -1,21 +1,32 @@
 package com.epam.hackerrank.data_structures.task1;
 
+import com.epam.hackerrank.data_structures.task5.BalancedBrackets;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ArraysLeftRotationTest {
+    private int[] initialArray = {1, 2, 3, 4, 5};
+    private int[] expectedArray = {5, 1, 2, 3, 4};
 
     @Test
     void rotateTest() throws Exception {
-        int[] initialArray = {1, 2, 3, 4, 5};
-
-        int[] expectedArray = {5, 1, 2, 3, 4};
-
         ArraysLeftRotation.rotate(initialArray, 4);
 
         assertArrayEquals(initialArray, expectedArray);
 
+    }
+
+    @Test
+    void testWrongArguments() throws Exception {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> ArraysLeftRotation.rotate(null, 4));
+
+        assertEquals(exception.getMessage(), "Illegal argument!");
+
+        exception = assertThrows(IllegalArgumentException.class, () -> ArraysLeftRotation.rotate(initialArray, -1));
+
+        assertEquals(exception.getMessage(), "Illegal argument!");
     }
 }
---
+++ src/test/java/com/epam/hackerrank/data_structures/task3/RansomNoteTest.java
@@ -0,0 +1,5 @@
+package com.epam.hackerrank.data_structures.task3;
+
+
+public class RansomNoteTest {
+}
