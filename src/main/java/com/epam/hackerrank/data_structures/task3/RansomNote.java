06833e5b5436d041aecf34568e1c6efd5e41ea6f
JurassicSPb[credential] <Jurassic5@yandex.ru>
2017-07-26 11:08:35 +0300

    task3 done

--- src/main/java/com/epam/hackerrank/data_structures/task3/RansomNote.java
+++ src/main/java/com/epam/hackerrank/data_structures/task3/RansomNote.java
@@ -1,5 +1,31 @@
 package com.epam.hackerrank.data_structures.task3;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RansomNote {
+    private Map<String, Integer> wordsMap = new HashMap<>();
+
+    public boolean solve(String magazine, String note) {
+        for (String s : magazine.split(" ")) {
+            if (s.length() <= 1 || s.length() > 5) {
+                return false;
+            }
+            wordsMap.putIfAbsent(s, 1);
+        }
+
+        for (String s : note.split(" ")) {
+            if (s.length() <= 1 || s.length() > 5) {
+                return false;
+            }
+            if (wordsMap.get(s) == null || wordsMap.get(s) == 0) {
+                return false;
+            } else {
+                wordsMap.put(s, 0);
+            }
+        }
+        System.out.println(wordsMap);
+        return true;
+    }
 }
--- src/test/java/com/epam/hackerrank/data_structures/task3/RansomNoteTest.java
+++ src/test/java/com/epam/hackerrank/data_structures/task3/RansomNoteTest.java
@@ -1,5 +1,52 @@
 package com.epam.hackerrank.data_structures.task3;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RansomNoteTest {
+    private RansomNote r = new RansomNote();
+    private RansomNote r2 = new RansomNote();
+
+    @BeforeEach
+    void init() throws Exception {
+        r = new RansomNote();
+        r2 = new RansomNote();
+    }
+
+    @Test
+    void ransomNoteTestReturnsTrue() throws Exception {
+        String magazine = "give me one grand today night";
+        String note = "give one grand today";
+
+        String magazine2 = "give me one grand today night hello world";
+        String note2 = "give one grand today world hello";
+
+        assertThat(r.solve(magazine, note), is(true));
+
+        assertThat(r2.solve(magazine2, note2), is(true));
+    }
+
+    @Test
+    void ransomNoteTestReturnsFalse() throws Exception {
+        String magazine = "two times three is not four";
+        String note = "two times two is four";
+
+        String magazine2 = "two times three is not four";
+        String note2 = "two times hello is four";
+
+        assertThat(r.solve(magazine, note), is(false));
+
+        assertThat(r.solve(magazine2, note2), is(false));
+    }
+
+    @Test
+    void wrongArgumentsTest() throws Exception {
+        String magazine = "twoooooo times three is not four";
+        String note = "twoooooo times is four";
+
+        assertThat(r.solve(magazine, note), is(false));
+    }
 }
